d5d8f3c3e63dc9f34a691ccd594ac66bc819dfc1
Nick Farrow <nick@nickfarrow.com>
2025-12-14 20:26:42 +1100

    wasm

---
+++ pkg/yushan_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const wasm_combine: (a: number, b: number) => [number, number, number, number];
+export const wasm_keygen_finalize: (a: number, b: number) => [number, number, number, number];
+export const wasm_keygen_round1: (a: number, b: number, c: number) => [number, number, number, number];
+export const wasm_keygen_round2: (a: number, b: number) => [number, number, number, number];
+export const wasm_sign: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number, number, number];
+export const wasm_sign_nonce: (a: number, b: number) => [number, number, number, number];
+export const init: () => void;
+export const test_wasm: () => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
